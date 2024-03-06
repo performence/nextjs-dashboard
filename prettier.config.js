417528ca16b5c5a56a81d51add475ae08873c415
张新臣 <1152307093@qq.com>
2024-03-07 00:58:39 +0800

    inti

--- prettier.config.js
+++ prettier.config.js
@@ -2,5 +2,7 @@ const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
   ...styleguide,
+  semi: false,
+  printWidth: 120,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
 };
