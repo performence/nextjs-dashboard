84cca36b728cc6e3050eac7a79d9eeae5e92306a
张新臣 <1152307093@qq.com>
2024-03-07 15:03:58 +0800

    111

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <p>customers page</p>
+import { fetchFilteredCustomers } from '@/app/lib/data'
+import CustomersTable from '@/app/ui/customers/table'
+
+interface SearchParams {
+  searchParams?: {
+    query?: string
+    page?: string
+  }
+}
+
+export default async function Page({ searchParams }: SearchParams) {
+  const query = searchParams?.query || ''
+
+  const customers = await fetchFilteredCustomers(query)
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  )
 }
